6d71d723a4fc0e427c35d6f6346ac5fb06f70046
Nico Rithner <nicorithner@gmail.com>
2023-06-03 10:58:31 -0600

    Add temporary styling for button

--- src/components/toggleButton/ToggleButtons.styled.ts
+++ src/components/toggleButton/ToggleButtons.styled.ts
@@ -1,1 +1,15 @@
-export {}
+import styled from "styled-components";
+
+export const ButtonSectionWrapper = styled.section`
+  border: 1px solid purple;
+  display: flex;
+`;
+export const ButtonWrapper = styled.section`
+  display: flex;
+  align-self: flex-end;
+`;
+
+export const Button = styled.button`
+  width: 300px;
+`;
+export const ButtonArrow = styled.img``;

1dcca375842909814d28789778fc3e9c1b75cda2
Nico Rithner <nicorithner@gmail.com>
2023-06-03 10:56:59 -0600

    Style time section. Correct font looks different

--- src/components/timeSection/TimeSection.styled.ts
+++ src/components/timeSection/TimeSection.styled.ts
@@ -1,1 +1,38 @@
-export {}
+import styled from "styled-components";
+import { toRem } from "../../globals/mixins";
+
+export const TimeInfoWrapper = styled.section`
+  border: 1px solid orange;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const GreetingWrapper = styled.div`
+  display: flex;
+`;
+
+export const SunMoonIcon = styled.img`
+  width: 22.34px;
+  height: 24px;
+  margin-right: ${toRem(16)};
+`;
+
+export const ClockWrapper = styled.div`
+  display: flex;
+  width: fit-content;
+`;
+
+export const TimeZone = styled.p`
+  align-self: flex-end;
+  margin-left: ${toRem(16)};
+  font-family: Inter;
+  font-size: ${toRem(40)};
+  font-weight: 300;
+  line-height: ${toRem(40)};
+  letter-spacing: 0px;
+`;
+
+export const Greeting = styled.h4``;
+export const Time = styled.h1``;
+export const CityCountry = styled.h3``;
--- src/components/timeSection/TimeSection.tsx
+++ src/components/timeSection/TimeSection.tsx
@@ -1,3 +1,27 @@
+import {
+  CityCountry,
+  ClockWrapper,
+  Greeting,
+  GreetingWrapper,
+  SunMoonIcon,
+  Time,
+  TimeInfoWrapper,
+  TimeZone,
+} from "./TimeSection.styled";
+import iconMoon from "../../assets/desktop/icon-moon.svg";
+
 export const TimeSection = () => {
-  return <h3>Time Section</h3>
-}
+  return (
+    <TimeInfoWrapper>
+      <GreetingWrapper>
+        <SunMoonIcon src={iconMoon}/>
+        <Greeting>GOOD MORNING, IT’S CURRENTLY</Greeting>
+      </GreetingWrapper>
+      <ClockWrapper>
+        <Time>23:14</Time>
+        <TimeZone>BST</TimeZone>
+      </ClockWrapper>
+      <CityCountry>IN LONDON, UK</CityCountry>
+    </TimeInfoWrapper>
+  );
+};

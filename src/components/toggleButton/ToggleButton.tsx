67a42cf1649f853913d0ce194750e74843970614
Nico Rithner <nicorithner@gmail.com>
2023-06-03 18:07:45 -0600

    Add toggleButton function

--- src/components/toggleButton/ToggleButton.tsx
+++ src/components/toggleButton/ToggleButton.tsx
@@ -1,19 +1,33 @@
 import {
+  BttnArrowCircle,
   Button,
   ButtonArrow,
   ButtonSectionWrapper,
-  ButtonWrapper,
+  ButtonText,
 } from "./ToggleButtons.styled";
 import arrowIconDown from "../../assets/desktop/icon-arrow-down.svg";
+import arrowIconUp from "../../assets/desktop/icon-arrow-up.svg";
+import { useState } from "react";
+
 export const ToggleButton = () => {
+  const [arrowIcon, setArrowIcon] = useState(arrowIconUp);
+  const [content, setContent] = useState("MORE");
+
+  const toggleButton = () => {
+    arrowIcon == arrowIconUp
+      ? setArrowIcon(arrowIconDown)
+      : setArrowIcon(arrowIconUp);
+    content == "MORE" ? setContent("LESS") : setContent("MORE");
+  };
+
   return (
     <ButtonSectionWrapper>
-      <ButtonWrapper>
-        <Button>
-          MORE
-          <ButtonArrow src={arrowIconDown} />
-        </Button>
-      </ButtonWrapper>
+      <Button onClick={toggleButton}>
+        <ButtonText>{content}</ButtonText>
+        <BttnArrowCircle>
+          <ButtonArrow src={arrowIcon} />
+        </BttnArrowCircle>
+      </Button>
     </ButtonSectionWrapper>
   );
 };

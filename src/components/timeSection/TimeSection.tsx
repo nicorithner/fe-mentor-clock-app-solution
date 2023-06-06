6fffc8d64dde1d17c3efc19fa42e3fabee642d75
Nico Rithner <nicorithner@gmail.com>
2023-06-06 00:32:31 -0600

    Set up dynamic rendering of live tiem data

--- src/components/timeSection/TimeSection.tsx
+++ src/components/timeSection/TimeSection.tsx
@@ -9,19 +9,40 @@ import {
   TimeZone,
 } from "./TimeSection.styled";
 import iconMoon from "../../assets/desktop/icon-moon.svg";
+import iconSun from "../../assets/desktop/icon-sun.svg";
+import { WorldTimeAPI } from "../../apis/worldTimeAPI";
+import Clock from "react-live-clock";
+import { useEffect, useState } from "react";
 
 export const TimeSection = () => {
+  const [cityAndCountry, setCityAndCountry] = useState<string>("CHICAGO, USA");
+  const [timeZoneCode, setTimeZoneCode] = useState("MST");
+  const [timeZone, setTimeZone] = useState("America/Chicago");
+  const [greeting, setGreeting] = useState("GOOD MORNING");
+  const icon: string = greeting == "GOOD MORNING" ? iconSun : iconMoon;
+
+  useEffect(() => {
+    WorldTimeAPI.get().then((data) => {
+      setCityAndCountry(data.cityAndCountry);
+      setTimeZoneCode(data.timeZoneCode);
+      setGreeting(data.greeting);
+      setTimeZone(data.timeZone);
+    });
+  }, []);
+
   return (
     <TimeInfoWrapper>
       <GreetingWrapper>
-        <SunMoonIcon src={iconMoon}/>
-        <Greeting>GOOD MORNING, IT’S CURRENTLY</Greeting>
+        <SunMoonIcon src={icon} />
+        <Greeting>{greeting}, IT’S CURRENTLY</Greeting>
       </GreetingWrapper>
       <ClockWrapper>
-        <Time>23:14</Time>
-        <TimeZone>BST</TimeZone>
+        <Time>
+          <Clock format={"HH:mm"} ticking={true} timezone={timeZone} />
+        </Time>
+        <TimeZone>{timeZoneCode}</TimeZone>
       </ClockWrapper>
-      <CityCountry>IN LONDON, UK</CityCountry>
+      <CityCountry>{cityAndCountry}</CityCountry>
     </TimeInfoWrapper>
   );
 };

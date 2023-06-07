b0d60f9dc082ba37f7e8fce34923de599893b8ca
Nico Rithner <nicorithner@gmail.com>
2023-06-06 19:51:25 -0600

    Set extra info display value from toggle button

--- src/components/toggleButton/ToggleButton.tsx
+++ src/components/toggleButton/ToggleButton.tsx
@@ -8,16 +8,19 @@ import {
 import arrowIconDown from "../../assets/desktop/icon-arrow-down.svg";
 import arrowIconUp from "../../assets/desktop/icon-arrow-up.svg";
 import { useState } from "react";
-import { TimeContext } from "../../globals/contexts/TimeContext";
+import {
+  TimeContext,
+  useTimeContext,
+} from "../../globals/contexts/TimeContext";
 
 export const ToggleButton = () => {
+  const { displayExtraInfo, setDisplayExtraInfo } = useTimeContext();
   const [arrowIcon, setArrowIcon] = useState<string>(arrowIconUp);
   const [content, setContent] = useState<string>("MORE");
-  const [displayState, setDisplayState] = useState<boolean>(false);
-  console.log(displayState);
+
 
   const handleToggleDisplay = () => {
-    setDisplayState(!displayState);
+    setDisplayExtraInfo(!displayExtraInfo);
     arrowIcon == arrowIconUp
       ? setArrowIcon(arrowIconDown)
       : setArrowIcon(arrowIconUp);
@@ -25,7 +28,6 @@ export const ToggleButton = () => {
   };
 
   return (
-    <TimeContext.Provider value={{ displayStateContext: displayState }}>
       <ButtonSectionWrapper>
         <Button onClick={handleToggleDisplay}>
           <ButtonText>{content}</ButtonText>
@@ -34,6 +36,5 @@ export const ToggleButton = () => {
           </BttnArrowCircle>
         </Button>
       </ButtonSectionWrapper>
-    </TimeContext.Provider>
   );
 };
